af9cc0eace302e357f17aedd6a19adc37feed138
Alpay Altinel <aaltinel>
2026-03-15 15:24:18 +0300

    Initial Commit

---
+++ MyFinance/MyFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinanceApp.swift
+//  MyFinance
+//
+//  Created by ALPAY's MacBook on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
